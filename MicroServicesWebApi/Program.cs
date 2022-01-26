658ccf1176441fd8172775b8d9b6c4d53821900f
LAPTOP-1A0N2URC\Bolejko <jaroslawbolejko85@gmail.com>
2022-01-26 21:18:09 +0100

    delete bulider.Services.AddControllersWithView

--- MicroServicesWebApi/Program.cs
+++ MicroServicesWebApi/Program.cs
@@ -18,8 +18,8 @@ var builder = WebApplication.CreateBuilder(args);
 //Dbcontext will be moved to sqlserwer later
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
-//builder.Services.AddControllers();
-builder.Services.AddControllersWithViews(options => { options.SuppressAsyncSuffixInActionNames = false; });
+builder.Services.AddControllers();
+//builder.Services.AddControllersWithViews(options => { options.SuppressAsyncSuffixInActionNames = false; });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddEndpointsApiExplorer();
